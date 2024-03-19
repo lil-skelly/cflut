b89807e29b700934690b5886f4a6fadb8cc3fa1e
skeleton <skeleton@arch.com>
2024-03-19 20:59:54 +0200

    Refactored file structure, modified processChunk and hexifyPixel to use a shared buffer for efficiency.

---
+++ libs/pixutils/pixutils.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+#include "../client/client.h"
+#include "../log/log.h"
+#include "pixutils.h"
+
+#include "../stb_image/stb_image.h"
+#include "../stb_image/stb_image_resize2.h"
+
+
+/**
+ * Convert a pixel's color to a hexadecimal string.
+ * @param x X-coordinate of the pixel.
+ * @param y Y-coordinate of the pixel.
+ * @param c Color of the pixel.
+ * @return A string containing the hexadecimal representation of the pixel's color.
+ */
+void hexifyPixel(int x, int y, color *c, char* buffer) {
+    snprintf(
+        buffer,
+        MAX_PIXEL_STRING_LENGTH,
+        "PX %d %d %02x%02x%02x%02x\n", 
+        x, 
+        y, 
+        c->r, 
+        c->g, 
+        c->b, 
+        c->a
+    );
+}
+/**
+ * Divides an image into the specified number of chunks.
+ *
+ * This function takes an image and a chunk count as input, and divides the image into that many chunks.
+ * Each chunk is represented by a `chunk` struct, which contains a start and end pointer (pointing to the image data),
+ * and x, y coordinates representing the top-left corner of the chunk in the image.
+ *
+ * @param image The image to be divided into chunks.
+ * @param chunkCount The number of chunks to divide the image into.
+ * @return A pointer to an array of `chunk` structs, each representing a chunk of the image.
+ */
+chunk* makeChunks(image image, int chunkCount) {
+    chunk* chunks = (chunk*)malloc(chunkCount * sizeof(chunk));
+    int imageSize = image.width * image.height;
+    int chunkSize = image.width / chunkCount;
+    log_info("IMAGE SIZE: %d, CHUNK SIZE: %d\n", imageSize, chunkSize);
+    int i;
+    for (i = 0; i < chunkCount; i++) {
+        int start = i * chunkSize;
+        int end = start + chunkSize;
+        chunks[i] = (chunk){
+            .x = 0,
+            .y = i * chunkSize,
+            .start = (color*)(image.originalImage + start * image.width * DEFAULT_CHANNELS), // removed * image.width
+            .end = (color*)(image.originalImage + end * image.width * DEFAULT_CHANNELS),
+        };
+    }
+    return chunks;
+}
+
+void processChunk(image image, chunk chunk, int chunkCount, SOCKET client) {
+    color* colorImage = (color*)image.originalImage;
+    int x, y;
+    log_info("Start address: %p\nEnd address: %p\n", (void*)chunk.start, (void*)chunk.end);
+    char pixelBuffer[MAX_PIXEL_STRING_LENGTH];
+    for (color* it = chunk.start; it < chunk.end; it++) {
+        // send pixel
+        int index = it - (color*)image.originalImage;
+        x = (index % image.width);
+        y = index / image.width;
+        hexifyPixel(x, y, it, pixelBuffer); //offset should be added at the X position
+        sendMessage(client, pixelBuffer);
+    }
+}
+/**
+ * Load image from file.
+ * @param filename Path to file.
+ * @return An image struct containing the loaded image.
+ */
+image loadImage(char* filename) {
+    int width, height, channels;
+    unsigned char *loadedImage = stbi_load(filename, &width, &height, &channels, 4);
+    if (loadedImage == NULL) {
+        log_error("Could not load image <%s>\n", filename);
+        exit(1);
+    }
+    log_info("[*] Loaded image <%s>\n", filename, width, height, channels);
+    image Image = {loadedImage, width, height, channels};
+    return Image;
+
+}
+
+/**
+ * Resize an image to the specified dimensions.
+ * @param image Pointer to the image to resize.
+ * @param width New width of the image.
+ * @param height New height of the image.
+ * @param channels Number of color channels in the image.
+ */
+void resizeImage(image *image, int width, int height, int channels) {
+    unsigned char* resized_image = (unsigned char*)malloc(width * height * channels * sizeof(unsigned char)); // Allocate the size of the resized image
+    stbir_resize_uint8_linear(
+        image->originalImage,
+        image->width,
+        image->height,
+        0,
+        resized_image,
+        width,
+        height,
+        0,
+        channels
+    );
+    free(image->originalImage); // Free memory of original image
+    // Modify original image object.
+    image->originalImage = resized_image;
+    image->width = width;
+    image->height = height;
+    image->channels = channels;
+    log_info("[*] RESIZED IMAGE -> W:%dpx H:%dpx C:%d\n", image->width, image->height, image->channels);
+}
+
+
+
+// END OF [FUNCTION IMPLEMENTATIONS]
---
+++ libs/pixutils/pixutils.h
@@ -0,0 +1,48 @@
+#ifndef PIXUTILS_H_
+#define PIXUTILS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAX_PIXEL_STRING_LENGTH 30
+#define DEFAULT_CHANNELS 4
+// [STRUCTURES]
+/**
+ * Structure to represent a color.
+ * Each color is represented by its 4 channels: red, green, blue, and alpha.
+ */
+typedef struct {
+    unsigned char r, g, b, a;
+} color;
+
+/**
+ * Structure to represent an image by its original stb object and its width, height and number of channels..
+ */
+typedef struct {
+    unsigned char *originalImage; // Use original image in order  to retrieve the pixel values to mitigate memory usage
+    int width, height, channels;
+} image;
+
+/**
+ * Structure to represent a chunk of an image.
+ * A chunk is represented by its start and end pointers (to the image) , and its x, y coordinates.
+ */
+typedef struct {
+    color *start, *end;
+    int x, y;
+} chunk;
+// END OF [STRUCTURES]
+
+// [FUNCTION DECLARATIONS]
+image loadImage(char* filename);
+void resizeImage(image *image, int width, int height, int channels);
+
+void hexifyPixel(int x, int y, color *c, char* buffer);
+
+chunk* makeChunks(image image, int chunk_count);
+void processChunk(image image, chunk chunk, int chunkCount, SOCKET client);
+// END OF [FUNCTION DECLARATIONS]
+#endif
